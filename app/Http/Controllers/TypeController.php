1a9e32dffe692d7eb615ef7912f05338e8c18bad
simoneditaranto <simodita99@gmail.com>
2024-04-23 17:53:14 +0200

    feat: CRUD type, softDelete( ), bonus completati

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -79,6 +79,8 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        return redirect()->route('admin.types.index');
     }
 }
--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -7,14 +7,14 @@
             <div class="card-body">
               <h2 class="card-title">{{$type->name}}</h2>
               
-            <a href="{{route('admin.types.edit', $type->id)}}" class="btn btn-warning">Modifica</a>
+                <a href="{{route('admin.types.edit', $type->id)}}" class="btn btn-warning">Modifica</a>
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Elmina progetto
+                </button>
             </div>    
-            {{-- <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Elmina progetto
-            </button> --}}
 
             <!-- Modal -->
-            {{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -22,12 +22,12 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            Sei sicuro di voler eliminare il fumetto "{{$project->name}}"?
+                            Sei sicuro di voler eliminare il futipometto "{{$type->name}}"?
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
 
-                            <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                            <form action="{{route('admin.types.destroy', $type->id)}}" method="POST">
                                 @csrf
                                 @method("DELETE")
 
@@ -38,11 +38,11 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
 
-        {{-- <button type="button" class="btn btn-primary">
-            <a href="/admin/projects" class="text-white text-decoration-none">Torna ai progetti</a>
-        </button> --}}
+        <button type="button" class="btn btn-primary">
+            <a href="/admin/types" class="text-white text-decoration-none">Torna ai progetti</a>
+        </button>
     </div>
 
 
